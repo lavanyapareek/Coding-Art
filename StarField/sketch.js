862e659015961bf10a7ee5ee631a8736de295c3f
lavanyapareek <lavanyapareek@gmail.com>
2023-02-04 11:47:43 +0530

    Added Colors

--- StarField/sketch.js
+++ StarField/sketch.js
@@ -1,9 +1,18 @@
+
+
+
 function Star() {
+  this.co = [[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255], [0,0,255], [255,0,0], [255,255,0], [255, 191, 0],[255,255,255],[255,255,255],[255,255,255]]
+  this.c = this.co[Math.floor(random(0,15))]
   this.x = random(-width, width)
   this.y = random(-height, height)
   this.z = random(width)
+  this.c1 = random(0, 255)
+  this.c2 = random(0, 255)
+  this.c3 = random(0, 255)
   this.update = function () {
       this.z = this.z - speed
+      
       if (this.z < 1) {
           this.z = width
           this.x = random(-width, width)
@@ -12,7 +21,8 @@ function Star() {
       }
   }
   this.show = function () {
-      fill(255)
+      var c = this.c
+      fill(c)
       noStroke()
       var sx = map(this.x / this.z, 0, 1, 0, width)
       var sy = map(this.y / this.z, 0, 1, 0, height)
@@ -21,17 +31,21 @@ function Star() {
       var px = map(this.x / this.pz, 0, 1, 0, width)
       var py = map(this.y / this.pz, 0, 1, 0, height)
       this.pz = this.z
-      stroke(255)
+      stroke(c)
       line(px, py, sx, sy)
   }
 }
 var stars = []
 var speed
+
 function setup(){
-  createCanvas(1920, 1080);
+  createCanvas(windowWidth, windowHeight);
   for(var i = 0; i < 2000; i++){
     stars[i] = new Star();
   }
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 function draw(){
   speed = map(mouseX, 0, width, 0, 100);
